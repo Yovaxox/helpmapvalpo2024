b4e211111012a1cd50d7c9f68feb4b2b74d6a3bd
javier.paredes@outlook.com <javier.paredes@outlook.com>
2024-02-03 23:49:27 -0300

    Update vite.config.js to use defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    // Agrega esto para manejar archivos .jsx
-    fs: {
-      strict: false,
-    },
-  },
-};
+})
